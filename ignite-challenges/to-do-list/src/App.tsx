d60910bb6e602a46d9438ad6249f1677c404ef5e
João Pedro <jpedrones@hotmail.com>
2025-01-15 12:29:03 -0300

    Add todo list inicial

---
+++ ignite-challenges/to-do-list/src/App.tsx
@@ -0,0 +1,17 @@
+import styles  from './App.module.css'
+
+import {  } from   ;
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className={styles.wrapper}>
+        {/* <Kanban /> */}
+        {/* <Footer /> */}
+      </div>
+    </>
+  )
+}
+
+export default App
