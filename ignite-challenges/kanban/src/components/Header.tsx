cd9c83ac669ba27db1658f6ec488263a750eb520
João Pedro <jpedrones@hotmail.com>
2025-01-17 23:42:31 -0300

    Add Header component

--- ignite-challenges/kanban/src/components/Header.tsx
+++ ignite-challenges/kanban/src/components/Header.tsx
@@ -4,12 +4,12 @@ import rocket  from '../assets/rocket.svg';
 
 export function Header() {
 	return(
-		<div className={styles.container}>
+		<section className={styles.container}>
 
 			<img src={rocket}/>
-			<span className={styles.blue}>Kan</span>
-			<span className={styles.purple}>ban</span>
+			<span className={styles.redBrick}>Kan</span>
+			<span className={styles.blueDark}>ban</span>
 
-		</div>
+		</section>
 	)
 }
