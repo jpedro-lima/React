0ec6489653de17bbc64c11b39a5d956cc5e6fb13
João Pedro <jpedrones@hotmail.com>
2025-02-01 14:36:28 -0300

    Add geral configs

--- timer/eslint.config.js
+++ timer/eslint.config.js
@@ -1,8 +1,7 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,6 +9,7 @@ export default [
 	{ languageOptions: { globals: globals.browser } },
 	pluginJs.configs.recommended,
 	...tseslint.configs.recommended,
+	tseslint.configs.stylisticTypeChecked,
 	pluginReact.configs.flat.recommended,
 	pluginReact.configs.flat['jsx-runtime'],
 	{
