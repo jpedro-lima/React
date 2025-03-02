6c6db73526df03858ee3fcd85c78083f96f1dbdd
João Pedro <jpedrones@hotmail.com>
2025-03-02 16:08:59 -0300

    Update axios and env to create daley in requests for tests

--- pizza-shop/src/env.ts
+++ pizza-shop/src/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 const envScheme = z.object({
 	VITE_API_URL: z.string().url(),
+	VITE_API_DALEY: z.string().transform(Number),
 })
 
 export const env = envScheme.parse(import.meta.env)
--- pizza-shop/src/lib/axios.ts
+++ pizza-shop/src/lib/axios.ts
@@ -5,3 +5,10 @@ export const api = axios.create({
 	baseURL: env.VITE_API_URL,
 	withCredentials: true,
 })
+
+if (env.VITE_API_DALEY) {
+	api.interceptors.request.use(async (config) => {
+		await new Promise((resolve) => setInterval(resolve, env.VITE_API_DALEY))
+		return config
+	})
+}
